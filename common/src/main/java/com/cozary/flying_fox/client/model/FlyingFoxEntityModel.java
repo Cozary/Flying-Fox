3964bf5a57ff6d32fcc80914ca04ee39a4825170
Cozary <ayrecio99@gmail.com>
2024-10-28 17:02:48 +0100

    change license to GPL-3.0-or-later

--- common/src/main/java/com/cozary/flying_fox/client/model/FlyingFoxEntityModel.java
+++ common/src/main/java/com/cozary/flying_fox/client/model/FlyingFoxEntityModel.java
@@ -1,3 +1,26 @@
+/*
+ *
+ *  *
+ *  *  * Copyright (c) 2024 Cozary
+ *  *  *
+ *  *  * This file is part of Flying Fox, a mod made for Minecraft.
+ *  *  *
+ *  *  * Flying Fox is free software: you can redistribute it and/or modify it
+ *  *  * under the terms of the GNU General Public License as published
+ *  *  * by the Free Software Foundation, either version 3 of the License, or
+ *  *  * (at your option) any later version.
+ *  *  *
+ *  *  * Flying Fox is distributed in the hope that it will be useful, but
+ *  *  * WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  *  * MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE.  See the
+ *  *  * GNU General Public License for more details.
+ *  *  *
+ *  *  * You should have received a copy of the GNU General Public License
+ *  *  * License along with Flying Fox.  If not, see <https://www.gnu.org/licenses/>.
+ *  *
+ *
+ */
+
 package com.cozary.flying_fox.client.model;
 
 import com.cozary.flying_fox.entities.FlyingFoxEntity;
